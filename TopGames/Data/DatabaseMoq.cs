abba8835f2e27ee19e47153e7942d644d3c495cc
Pavel Apasov <apasovp@seznam.cz>
2023-11-16 21:07:40 +0100

    Add(MiniSlider and MiniSliderCard +HTML/CSS) Update DatabaseMoq + GameModel

--- TopGames/Data/DatabaseMoq.cs
+++ TopGames/Data/DatabaseMoq.cs
@@ -7,7 +7,6 @@ namespace TopGames.Data
         public static List<SliderModel> Slides
         {
             get
-
             {
                 List<SliderModel> slides = new List<SliderModel>()
                 {
@@ -21,44 +20,106 @@ namespace TopGames.Data
                     },
                     new SliderModel()
                     {
-                        Title = "Alan Wake 2",
+                        Title = "Assassin’s Creed Mirage",
                         Url="2",
-                        Image = "https://cdn2.unrealengine.com/egs-alan-wake-2-carousel-thumb-1200x1600-c64beec29b55.jpg?h=128&ampquality=medium&amp;resize=1&amp;w=96"
+                        Image = "https://cdn2.unrealengine.com/egs-ac-mirage-master-assassin-carousel-thumb-1200x1600-04b784b5228f.jpg?h=128&ampquality=medium&ampresize=1&ampw=96"
                     },
                     new SliderModel()
                     {
-                        Title = "Alan Wake 2",
+                        Title = "Return to Moria™",
                         Url="3",
-                        Image = "https://cdn2.unrealengine.com/egs-alan-wake-2-carousel-thumb-1200x1600-c64beec29b55.jpg?h=128&ampquality=medium&amp;resize=1&amp;w=96"
+                        Image = "https://cdn2.unrealengine.com/egs-lotr-return-to-moria-carousel-thumb-v2-1200x1600-5d5f6bd3b206.jpg?h=128&quality=medium&resize=1&w=96"
                     },
                     new SliderModel()
                     {
-                        Title = "Alan Wake 3",
+                        Title = "Honkai: Star Rail",
                         Url="4",
-                        Image = "https://cdn2.unrealengine.com/egs-alan-wake-2-carousel-thumb-1200x1600-c64beec29b55.jpg?h=128&ampquality=medium&amp;resize=1&amp;w=96"
+                        Image = "https://cdn2.unrealengine.com/en-egs-honkai-star-rail-1-5-carousel-thumb-1200x1600-85873137956e.jpg?h=128&quality=medium&resize=1&w=96"
                     },
                     new SliderModel()
                     {
-                        Title = "Alan Wake 2",
+                        Title = "Suicide Squad: Kill the Justice League - Deluxe Edition",
                         Url="5",
-                        Image = "https://cdn2.unrealengine.com/egs-alan-wake-2-carousel-thumb-1200x1600-c64beec29b55.jpg?h=128&ampquality=medium&amp;resize=1&amp;w=96"
+                        Image = "https://cdn2.unrealengine.com/egs-suicide-squad-ktjl-deluxe-carousel-thumb-1200x1600-bd5a43909d22.jpg?h=128&quality=medium&resize=1&w=96"
                     },
                     new SliderModel()
                     {
-                        Title = "Alan Wake 2",
+                        Title = "Banishers: Ghosts of New Eden",
                         Url="6",
-                        Image = "https://cdn2.unrealengine.com/egs-alan-wake-2-carousel-thumb-1200x1600-c64beec29b55.jpg?h=128&ampquality=medium&amp;resize=1&amp;w=96"
+                        Image = "https://cdn2.unrealengine.com/egs-banishers-ghosts-of-new-eden-carousel-mobile-thumb-1200x1600-976575969b7b.jpg?h=128&quality=medium&resize=1&w=96"
                     }
 
                 };
-
-
-
                 return slides;
-
-
-
             }
         }
+
+
+        public static List<GameModel> Games { get; set; }
+
+        static DatabaseMoq()
+        {
+
+             Games = new List<GameModel>()
+                {
+
+
+                    new GameModel()
+                    {
+                        Id= Guid.NewGuid(),
+                        Title = "Car Mechanic - City Driving",
+                        UrlImage="https://cdn1.epicgames.com/spt-assets/be29ee7e2f14449d822b76bf52e8f5ab/car-mechanic--city-driving-17nkn.png?h=480&quality=medium&resize=1&w=360",
+                        Price=250,
+                        Discount=50
+
+                    },
+                    new GameModel()
+                    {
+                         Id= Guid.NewGuid(),
+                        Title = "Achilles Legends Untold",
+                        UrlImage="https://cdn1.epicgames.com/spt-assets/aec31192b8a6463c9bd43dcb6ece5c8f/achilles-legends-untold-trjaz.jpg?h=480&quality=medium&resize=1&w=360",
+                        Price=530,
+                        Discount=30
+
+                    },
+                    new GameModel()
+                    {
+                         Id= Guid.NewGuid(),
+                        Title = "Hanoi Puzzles - Solid Match",
+                        UrlImage="https://cdn1.epicgames.com/spt-assets/58e43b2cb77d4087882873f43831b6a1/hanoi-puzzles--solid-match-c5eqt.png?h=480&quality=medium&resize=1&w=360",
+                        Price=239,
+                        Discount=33
+
+                    },
+                    new GameModel()
+                    {
+                         Id= Guid.NewGuid(),
+                        Title = "Ghoul Castle 3D - Gold Edition",
+                        UrlImage="https://cdn1.epicgames.com/spt-assets/2402ff8ccf5647828483c9e8880e3cce/ghoul-castle-3d--gold-edition-rchv0.png?h=480&quality=medium&resize=1&w=360",
+                        Price=659,
+                        Discount=20
+                    },
+                    new GameModel()
+                    {
+                         Id= Guid.NewGuid(),
+                        Title = "Island Farmer",
+                        UrlImage="https://cdn1.epicgames.com/spt-assets/3fb5be523b7a46d39f9b2da081fad744/island-farmer-1keo0.jpg?h=480&quality=medium&resize=1&w=360",
+                        Price=759,
+                        Discount=10
+                    },
+                    new GameModel()
+                    {
+                        Id= Guid.NewGuid(),
+                        Title = "GINSHA",
+                        UrlImage="https://cdn1.epicgames.com/spt-assets/d9cf30b30233475dbcf56055db33c826/ginsha-11fm9.jpg?h=480&quality=medium&resize=1&w=360",
+                        Price=450,
+                        Discount=43
+                    }
+                };
+            
+        }
+        
+            
+        
     }
 }
---
+++ TopGames/Models/GameModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TopGames.Models
+{
+    public class GameModel
+    {
+        public Guid Id { get; set; }
+
+        public string UrlImage { get; set; }
+
+        public string Title { get; set; }   
+
+        public double Discount { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
